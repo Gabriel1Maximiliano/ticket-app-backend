5cc9e0b57b8a4a6f17d5457669dc12002cbcc639
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-03-21 17:16:55 -0300

    callback to create new ticket was implemented

--- models/sockets.js
+++ models/sockets.js
@@ -1,12 +1,12 @@
-const BandList = require('./band-list');
+const TicketList = require('./ticket-list');
 
 class Sockets {
 
-    constructor(io) {
+    constructor( io ) {
 
         this.io = io;
         this.socketEvents();
-        this.bandList = new BandList();
+        this.ticketList = new TicketList();
     }
     socketEvents() {
         // On Connection
@@ -14,36 +14,17 @@ class Sockets {
 
             console.log('cliente conectado')
 
-            // emitir al cliente conectado todas las bandas
-            socket.emit('current-bands', this.bandList.getBands());
+          
+
+          socket.on('request-ticket', ( data,callback ) => {
+
+           const newTicket = this.ticketList.createTicket();
+           callback( newTicket );
+           
+          })
 
 
-            // vote for the band
-            socket.on('band-vote', (id) => {
-                this.bandList.increaseVotes(id);
-                this.io.emit('current-bands', this.bandList.getBands());
-            })
-
-            socket.on('delete-band', (id) => {
-                this.bandList.removeBand(id);
-                this.io.emit('current-bands', this.bandList.getBands());
-
-            })
-            socket.on('change-band-name', ({id ,name}) => {
-               
-                this.bandList.changeName( id,name )
-                this.io.emit('current-bands', this.bandList.getBands());
-
-            })
-            socket.on('create-new-band', ({name}) => {
-               
-                this.bandList.addBand( name )
-                this.io.emit('current-bands', this.bandList.getBands());
-
-            })
-
-
-        })
+        });
 
 
 
